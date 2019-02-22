96da95e207735bd3d9e3327e94df90a5797fd912
Ivo Calado <ivo.calado@ifal.edu.br>
2019-02-22 00:26:06 -0300

    Adiciona o supore filtragem de requisições
    Signed-off-by: Ivo Calado <ivo.calado@ifal.edu.br>

--- src/app/usuarios/usuario-list/usuario-list.component.ts
+++ src/app/usuarios/usuario-list/usuario-list.component.ts
@@ -5,6 +5,7 @@ import { SnackBarService } from '../../_shared/helpers/snackbar.service';
 import { AuthenticationDataService } from '../../_shared/services/authentication-data.service';
 import { Usuario } from '../../_shared/models/usuario.model';
 import { UnidadeDataService } from '../../_shared/services/unidade-data.service';
+import { UsuarioDataService } from '../../_shared/services/usuario-data.service';
 
 
 @Component({
@@ -13,21 +14,30 @@ import { UnidadeDataService } from '../../_shared/services/unidade-data.service'
     styleUrls: ["./usuario-list.component.scss"]
 })
 export class UsuarioListComponent implements OnInit {
-    @ViewChild(MatSort) sort: MatSort;
+    @ViewChild(MatSort) sortUnidade: MatSort;
 
     //Estrutura de dados para exibição dos usuarios da unidade
-    displayedColumns: string[] = ["nome", "email", "tipo", "actions"];
-    dataSource;
+    displayedColumnsUnidade: string[] = ["nome", "email", "tipo", "actions"];
+    dataSourceUnidade;
+
+    @ViewChild(MatSort) sortGeral: MatSort;
+
+    //Estrutura de dados para exibição dos usuarios da unidade
+    displayedColumnsGeral: string[] = ["nome", "email", "tipo", "actions"];
+    dataSourceGeral;
 
 
     usuarioAutenticado: Usuario; 
+    usuariosUnidade: Usuario[]
+    todosUsuarios: Usuario[]
 
     constructor(
         private route: ActivatedRoute,
         private router: Router,
         private snackBarService: SnackBarService,
         private authenticationDataService: AuthenticationDataService,
-        private unidadeDataService: UnidadeDataService
+        private unidadeDataService: UnidadeDataService,
+        private usuarioDataService: UsuarioDataService
     ) {}
 
     ngOnInit() {
@@ -49,9 +59,34 @@ export class UsuarioListComponent implements OnInit {
     fetchUsuarios() {
         this.unidadeDataService.getUnidade(this.usuarioAutenticado.unidade_id).subscribe(unidade => {
             this.unidadeDataService.getUsuariosByUnidade(this.usuarioAutenticado.id).subscribe((usuarios:Usuario[])  => {
-                this.dataSource = new MatTableDataSource(usuarios);
-                this.dataSource.sort = this.sort;
+                console.log(usuarios)
+                this.usuariosUnidade= usuarios
+                this.dataSourceUnidade = new MatTableDataSource(usuarios);
+                this.dataSourceUnidade.sort = this.sortUnidade;
+
+                let usuariosGeralExibir = []
+                if(this.usuarioAutenticado.unidade.classe == "admin") {
+                    this.usuarioDataService.getUsuarios().subscribe(todosUsuarios => {
+                        this.todosUsuarios = todosUsuarios
+
+                        
+                        for (let elem of this.todosUsuarios) {
+                            if(!this.usuariosUnidade.includes(elem)) {
+                                usuariosGeralExibir.push(elem)
+                            }
+                        }
+                    })
+
+                }
+                this.dataSourceGeral = new MatTableDataSource(usuariosGeralExibir)
+                this.dataSourceGeral = this.sortGeral
+        
+
             })
         })
+        // this.usuarioDataService.getUsuarios().subscribe(usuarios => {
+        //     this.dataSourceUnidade = new MatTableDataSource(usuarios)
+        //     this.dataSourceUnidade.sort = this.sortUnidade
+        // })
     }
 }
