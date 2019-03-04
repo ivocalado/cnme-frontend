2a28d6b5fdbaf1dc7fb275ec4d47e01c5ae6a2bd
Ivo Calado <ivo.calado@ifal.edu.br>
2019-03-04 08:09:04 -0300

    Adiciona tela de detalhamento de dados de usuário
    Signed-off-by: Ivo Calado <ivo.calado@ifal.edu.br>

--- src/app/usuarios/usuario-details/usuario-details.component.ts
+++ src/app/usuarios/usuario-details/usuario-details.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { UsuarioDataService } from 'src/app/_shared/services/usuario-data.service';
+import { ActivatedRoute, Router, Params } from '@angular/router';
+import { Usuario } from 'src/app/_shared/models/usuario.model';
+import { AuthService } from 'src/app/_shared/services/auth.service';
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'app-usuario-details',
@@ -7,9 +12,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsuarioDetailsComponent implements OnInit {
 
-  constructor() { }
+  usuario: Usuario
+  constructor(private usuarioDataService: UsuarioDataService, 
+    private authService: AuthService, 
+    private route:ActivatedRoute,
+    private router:Router,
+    private location: Location) { }
 
   ngOnInit() {
+    this.usuario = Usuario.EMPTY_MODEL
+    this.route.params.subscribe((params:Params) =>{
+      const usuarioId = +params["id"];
+      this.usuarioDataService.getUsuario(usuarioId, this.authService.getToken()).subscribe((usuario:Usuario) =>{
+          this.usuario = usuario;
+      })
+    })
+  }
+
+  onCancel() {
+    this.location.back()
   }
 
 }
--- src/app/usuarios/usuarios.module.ts
+++ src/app/usuarios/usuarios.module.ts
@@ -8,6 +8,9 @@ import { SharedModule } from '../_shared/shared.module';
 import { MatFormFieldModule, MatInputModule, MatButtonModule, MatSelectModule, MatCardModule, MatTableModule } from '@angular/material';
 import { FormsModule } from '@angular/forms';
 import { UsuarioDetailsComponent } from './usuario-details/usuario-details.component';
+import { UsuarioDataService } from '../_shared/services/usuario-data.service';
+import { AuthService } from '../_shared/services/auth.service';
+import { NgxMaskModule } from "ngx-mask";
 
 @NgModule({
     declarations: [UsuariosComponent, UsuarioListComponent, UsuarioEditComponent, UsuarioDetailsComponent],
@@ -20,7 +23,9 @@ import { UsuarioDetailsComponent } from './usuario-details/usuario-details.compo
         MatButtonModule,
         MatCardModule,
         MatTableModule,
-        UsuariosRoutingModule
-    ]
+        UsuariosRoutingModule,
+        NgxMaskModule.forRoot()
+    ],
+    providers: [UsuarioDataService, AuthService]
 })
 export class UsuariosModule { }
